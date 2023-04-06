f5538491b0022bbb2b918ae3f06cc6a1e7ba0635
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-04-05 20:35:29 -0600

    Como extraigo un pedaso de la img?

--- imgPix.py
+++ imgPix.py
@@ -3,6 +3,14 @@ Programa equivalente al de Matlab solo que usando OpenCV (4.7.0)
 """
 #Importamos las bibliotecas necesarias
 import  cv2
+import numpy as np
+
+#Leer una imagen
+img=cv2.imread('p1.jpg')
+img1=np.array(img)
+
+#Mostramos una parte de la imagen
+print(img1[0,0])
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
