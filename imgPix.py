bb252a6437c0b177746d8abf3f5b3ebf935f2ac1
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-04-06 15:12:04 -0600

    Ver como crear una array vacio

--- imgPix.py
+++ imgPix.py
@@ -8,9 +8,23 @@ import numpy as np
 #Leer una imagen
 img=cv2.imread('p1.jpg')
 img1=np.array(img)
+img1_1=0
+
+#Matriz de ceros
+for i in range(0,260):
+    for j in range(0,320):
+        for z in range(0,3):
+            img1_1[i,j,z]=0
+img1_1=np.array(img1_1)
 
 #Mostramos una parte de la imagen
-print(img1[0,0])
+i=360
+j=420
+for i in range(321):
+    for j in range(741):
+        img1_1[i-360,j-420,:]=img1[i,j,:]
+cv2.imshow('Imagen',img)
+cv2.imshow('Fragmento',img1_1)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
